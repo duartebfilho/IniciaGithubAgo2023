f778ec809816550f3c5c56157e6b019d59374ac7
duartebfilho <duartebfilho@yahoo.com>
2023-08-09 13:39:58 +0200

    Versao 2, classe Falkengurger

---
+++ src/entities/Falkenburger.java
@@ -0,0 +1,27 @@
+package entities;
+
+public class Falkenburger {
+	private String alves;
+	private Integer numero;
+	public Falkenburger(String alves, Integer numero) {
+		super();
+		this.alves = alves;
+		this.numero = numero;
+	}
+	public String getAlves() {
+		return alves;
+	}
+	public void setAlves(String alves) {
+		this.alves = alves;
+	}
+	public Integer getNumero() {
+		return numero;
+	}
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+	
+	
+	
+
+}
